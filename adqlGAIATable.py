eec4aa14c561f24ce716945a882a12261d3fdf90
Richard Ashley <rashley@gmail.com>
2018-06-27 23:58:09 +0100

    gaia better

--- adqlGAIATable.py
+++ adqlGAIATable.py
@@ -52,6 +52,9 @@ if __name__ == "__main__":
     print("Executing query: " + query)
     job = Gaia.launch_job_async(query)
     results = job.get_results()
+
+    results.write("gaiasample.fits", format="fits")
+
     resultsTable = gaiaClass.gaiaTABLE()
     resultsTable.setColumns(columns)
     for count, r in enumerate(results):
@@ -62,57 +65,3 @@ if __name__ == "__main__":
     print('%d items added to the table.'%resultsTable.getLength())
     resultsTable.writeAsCSV('gaiasample.csv')
     sys.exit()
-
-
-
-    inputObjects = []
-    if arg.list:
-        listFile = open(arg.object, 'rt')
-        for line in listFile:
-            if line[0]=='#': continue
-            filename = line.strip()
-            print(filename)
-            inputObjects.append(filename)
-        listFile.close()
-    else:
-        inputObjects.append(arg.object)
-
-    if arg.columns=="show":
-        print(getDR2Columns())
-        sys.exit()
-
-    columns = []
-    columnFile = open(arg.columns)
-    for line in columnFile:
-        if line[0]=='#': continue
-        if len(line.strip())<1: continue
-        columnname = line.strip()
-        columns.append(columnname)
-    columnFile.close()
-    print(columns)
-
-    if arg.object=='random':
-        numObjects = 100
-        print("Getting %d random objects"%numObjects)
-        keys, results = getRandomVizier(numObjects)
-        randomTable = gaiaClass.gaiaTABLE()
-        for r in results: 
-            print(r['DR2Name'], r['Plx'], r['e_Plx'], r['Plx']/r['e_Plx'])
-            if r['Plx']/r['e_Plx'] > 20: 
-                randomTable.addItem('random', keys, r)
-        print("Found %d eligible objects"%randomTable.getLength())
-        randomTable.setColumns(columns)
-        randomTable.writeAsCSV('sample.csv')
-        sys.exit()
-
-    resultsTable = gaiaClass.gaiaTABLE()
-    resultsTable.setColumns(columns)
-    for inputObject in inputObjects:
-        ra, dec = getSimbadCoordinates(inputObject)
-        (simRA, simDEC) = parseCoords(ra, dec)
-        print("Name: %s    SIMBAD RA: %f, DEC: %f"%(inputObject, simRA, simDEC))
-        keys, closestMatch = getUniqueVizierResult(inputObject, 10)
-        resultsTable.addItem(inputObject, keys, closestMatch)
-
-    resultsTable.dumpTable()
-    resultsTable.writeAsCSV('pcebs.csv')
--- drawHRdiagram.py
+++ drawHRdiagram.py
@@ -3,10 +3,10 @@ import astropy, astroquery, sys, numpy
 import matplotlib.pyplot
 from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
-from astroquery.gaia import Gaia
+from astropy.io import fits
 import gaiaClass
 import generalUtils
-
+import mpl_scatter_density
 import argparse
 
 class gaiaTarget():
@@ -46,12 +46,72 @@ class gaiaTarget():
     def rejectMe(self):
         if self.data['phot_bp_mean_mag'] == '--': return True
         if self.data['phot_rp_mean_mag'] == '--': return True
+        if self.data['parallax'] == '--': return True
         return False
 
+
+class gaiaData():
+    def __init__(self, data=None):
+        self.data = data
+        self.columns = None
+
+    def setColumns(self, columns):
+        self.columns = columns
+
+    def setData(self, data):
+        self.data = data
+
+    def loadFromFITS(self, filename):
+        hdu = fits.open(arg.sources)
+        data = hdu[1].data # assuming the first extension is a table
+        columns = hdu[1].columns
+        hdu.close()
+        self.setColumns(columns)
+        self.setData(data)
+        return len(data)
+    
+    def showColumns(self):
+        if self.columns is None:
+            return None
+        retStr = ""
+        for c in self.columns.names:
+            retStr+= str(c) + " \n" 
+        return retStr
+
+    def computeDistance(self):
+        distances = [1000/parallax for parallax in self.data['parallax']]
+        print(self.data['parallax'])
+        self.distances = distances
+
+    def computeAbsG(self):
+        gmag = self.data['phot_g_mean_mag']
+        parallax = self.data['parallax']
+        absG = [ float(gmag + 5 * numpy.log10(parallax/100)) for gmag, parallax in zip(gmag, parallax) ]
+        self.absG = absG
+
+    def computeColour(self):
+        colours = [float(bp - rp) for bp, rp in zip(self.data['phot_bp_mean_mag'], self.data['phot_rp_mean_mag']) ]
+        self.colours = colours
+        
+    def filter(self):
+        newColours = []
+        newG = []
+        for colour, g in zip(self.colours, self.absG):
+            print(colour, g)
+            if numpy.isnan(colour): continue
+            if numpy.isnan(g): continue
+            newColours.append(colour)
+            newG.append(g)
+        self.colours = newColours
+        self.absG = newG
+
+
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Draws an HR diagram based on saved GAIA data.')
     parser.add_argument('sources', type=str, help='File containing a table of GAIA data.')
+    parser.add_argument('--extra', type=str, help='File containing a table of special objects.' )
     
     parser.add_argument('--version', action='store_true', help='Show Astropy and Astroquery versions.')
     arg = parser.parse_args()
@@ -70,58 +130,85 @@ if __name__ == "__main__":
     matplotlib.rcParams.update(params)
 
 
-    # Load the CSV table
-    dataFile = open(arg.sources, "rt")
-    
-    targets = []
-    lineCounter = 0
-    headings = []
-    for line in dataFile:
-        if len(line)<1: continue
-        if line[0]=='#': continue
-        params = line.strip().split(',')
-        if lineCounter==0:
-            for p in params:
-                headings.append(str(p.strip()))
-        else:
-            data = []
-            for p in params:
-                try:
-                    value = float(p)
-                except ValueError:
-                    value = str(p.strip())
-                data.append(value)
-            target = gaiaTarget()
-            target.addData(headings, data)
-            targets.append(target)
-        lineCounter+= 1
-    print(str(lineCounter-1) + " lines read from " + arg.sources)
+    sampleGaiaData = gaiaData()
+    numRows = sampleGaiaData.loadFromFITS(arg.sources)
+    print("Loaded %d rows of Gaia data."%numRows)
+    print(sampleGaiaData.showColumns())
+    sampleGaiaData.computeDistance()
+    sampleGaiaData.computeAbsG()
+    sampleGaiaData.computeColour()
 
-    filteredTargets = []
-    for t in targets:
-        if not t.rejectMe(): filteredTargets.append(t)
-
-    targets = filteredTargets
-
-    for index, t in enumerate(targets):
-        print(index, end=": ")
-        t.computeDistance()
-        t.computeAbsoluteG()
-        t.computeColour()
-        print(t)
-    
     HRdiagram = matplotlib.pyplot.figure(figsize=(11, 8))
 
-    absG = [t.data['absG'] for t in targets]
-    colours = [t.data['colour'] for t in targets]
+    sampleGaiaData.filter()
+    absG = sampleGaiaData.absG
+    colours = sampleGaiaData.colours
+    from astropy.visualization import LogStretch
+    from astropy.visualization import SqrtStretch
+    from astropy.visualization.mpl_normalize import ImageNormalize
+    norm = ImageNormalize(stretch=SqrtStretch())
 
-    matplotlib.pyplot.scatter(colours, absG, marker=".", alpha=0.25, color='grey')
+    ax = matplotlib.pyplot.gcf().add_subplot(1, 1, 1, projection='scatter_density')
+    ax.scatter_density(colours, absG, norm=norm, color="black")
+    #matplotlib.pyplot.scatter(colours, absG, marker=".", alpha=0.5, color='grey')
     matplotlib.pyplot.gca().invert_yaxis()
 
-	
+    
+    if arg.extra is not None:
+        dataFile = open(arg.extra, 'rt')
+        targets = []
+        lineCounter = 0
+        headings = []
+        for line in dataFile:
+            if len(line)<1: continue
+            if line[0]=='#': continue
+            params = line.strip().split(',')
+            if lineCounter==0:
+                for p in params:
+                    headings.append(str(p.strip()))
+            else:
+                data = []
+                for p in params:
+                    try:
+                        value = float(p)
+                    except ValueError:
+                        value = str(p.strip())
+                    data.append(value)
+                target = gaiaTarget()
+                target.addData(headings, data)
+                targets.append(target)
+            lineCounter+= 1
+        print(headings)
+        print(str(lineCounter-1) + " lines read from " + arg.extra)
+        
+        for t in targets:
+            print(t)
+        
+        filteredTargets = []
+        for t in targets:
+            if not t.rejectMe(): filteredTargets.append(t)
+
+        targets = filteredTargets
+
+        for index, t in enumerate(targets):
+            t.computeDistance()
+            t.computeAbsoluteG()
+            t.computeColour()
+    
+        absG = [t.data['absG'] for t in targets]
+        colours = [t.data['colour'] for t in targets]
+    
+        matplotlib.pyplot.scatter(colours, absG, color='k', marker='.')
+        names = [t.data['Name'][2:] for t in targets]
+        for i, txt in enumerate(names):
+            ax.annotate(txt, (colours[i],absG[i]))
+
+
+    matplotlib.pyplot.ylabel('$\mathrm{M}_{G}$')
+    matplotlib.pyplot.xlabel('$G_{BP} - G_{RP}$')
+
+    matplotlib.pyplot.savefig("hr_diagram.pdf")
+
     matplotlib.pyplot.show(block = False)
 
     input("Press enter to continue")
-
-
-
